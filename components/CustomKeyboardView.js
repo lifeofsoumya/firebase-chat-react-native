cd960fb4be4340b19c4887e1e61ab6ffb861fc74
lifeofsoumya <mondalsoumya2001@gmail.com>
2024-07-21 00:50:05 +0530

    keyboard view now has a config based on boolean wheater to take whole screen or not

--- components/CustomKeyboardView.js
+++ components/CustomKeyboardView.js
@@ -3,16 +3,24 @@ import React from 'react'
 
 const ios = Platform.OS=='ios'
 
-export default function CustomKeyboardView( {children} ) {
+export default function CustomKeyboardView( {children, inChat} ) {
+  let kBAViewConfig = {}
+  let sViewConfig = {};
+  if(inChat){
+    kBAViewConfig= {keyboardVerticalOffset: 90}
+    sViewConfig= {contentContainerStyle: {flex: 1}}
+  }
   return (
     <KeyboardAvoidingView
         behavior={ios ? 'padding' : 'height '}
         style={{flex: 1}}
+        {...kBAViewConfig}
     >
         <ScrollView
             style={{flex: 1}}
             bounces={false}
             showsVerticalScrollIndicator={false}
+            {...sViewConfig}
         >
             {children}
         </ScrollView>
