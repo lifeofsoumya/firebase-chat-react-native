3b6fdaabb9836e8cb0b899d916e6ce1efffd9dbc
lifeofsoumya <mondalsoumya2001@gmail.com>
2024-07-21 00:50:32 +0530

    fetch all users, list chats

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,19 +1,46 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useAuth } from '../../context/authContext'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { StatusBar } from 'expo-status-bar';
+import ChatList from "../../components/ChatList"
+import Loader from '../../components/Loader';
+import { getDocs, query, where } from 'firebase/firestore';
+import { usersRef } from '../../firebaseConfig';
 
-export default function home() {
+export default function Home() {
   const { logout, user } = useAuth();
-  const handleLogout = async (e) => {
-    await logout()
+  const [ users, setUsers ] = useState([])
+
+  async function getUsers(){
+    console.log('inside get users')
+    const q = query(usersRef, where("userId", "!=", user?.uid));
+    const querySnapShot = await getDocs(q);
+    let data = [];
+    querySnapShot.forEach(doc => {
+      data.push({...doc.data()})
+    })
+    setUsers(data);
   }
+
+  useEffect(()=> {
+    if(user?.uid){
+      getUsers();
+    }
+  }, [user])
+
   return (
     <View> 
-      <Text>Protected Route</Text>
-      <Text>{user?.userName}</Text>
-      <TouchableOpacity onPress={handleLogout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
+      <StatusBar style='light' />
+      {
+        users.length > 0 ? (
+            <ChatList users={users}/>
+        ) : (
+          <View className="flex-1 items-center" style={{top: hp(30)}}>
+            <Loader size={hp(10)} />
+          </View>
+        )
+      }
     </View>
   )
 }
