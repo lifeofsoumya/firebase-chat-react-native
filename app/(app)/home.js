65c2bfb96c21f0636d94a1f4e2b44e7a41b2e687
lifeofsoumya <mondalsoumya2001@gmail.com>
2024-07-20 20:32:24 +0530

    update user data, login register with error codes handled

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useAuth } from '../../context/authContext'
 
 export default function home() {
+  const { logout, user } = useAuth();
+  const handleLogout = async (e) => {
+    await logout()
+  }
   return (
-    <View>
+    <View> 
       <Text>Protected Route</Text>
+      <Text>{user?.userName}</Text>
+      <TouchableOpacity onPress={handleLogout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- context/authContext.js
+++ context/authContext.js
@@ -1,6 +1,6 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { createContext, useEffect, useContext, useState } from "react";
-import { auth } from "../firebaseConfig";
+import { auth, db } from "../firebaseConfig";
 import { getDoc, setDoc, doc } from "firebase/firestore";
 
 
@@ -15,39 +15,66 @@ export const AuthContextProvider = ({ children }) => {
             if(user) {
                 setIsAuthenticated(true);
                 setUser(user)
+                updateUserData(user?.uid)
             }
             else setIsAuthenticated(false)
         })
         return unSub
     }, [])
 
+    const updateUserData = async(userId) => {
+        const userDataRef = doc(db, "users", userId);
+        const userDocSnapshot = await getDoc(userDataRef)
+
+        if(userDocSnapshot.exists()){
+            let data = userDocSnapshot.data();
+            setUser({...user, userName: data.userName, userId: data.userId, email: data.email})
+        }
+    }
+
     const login = async(email, password) => {
         try {
-            
+            const res = await signInWithEmailAndPassword(auth, email, password);
+            return { success: true }
         } catch (error) {
-            
+            if(error.message == 'Firebase: Error (auth/invalid-email).') return { success: false, message: "Enter valid email"}
+            if(error.message == 'Firebase: Error (auth/invalid-credential).') return { success: false, message: "Invalid credential"}
+            return { success: false, message: error.message }
         }
     }
 
     const logOut = async() => {
         try {
-            
+            await signOut(auth)
+            setIsAuthenticated(undefined);
+            setUser(null)
+            return { success: true }
         } catch (error) {
-            
+            return { success: false, message: error.message}
         }
     }
 
-    const register = async(email, password, name, profilePic) => {
+    const register = async(email, password, userName, profilePic) => {
         try {
             const res = await createUserWithEmailAndPassword(auth, email, password) 
             console.log('res user', res?.user)
+
+            await setDoc(doc(db, "users", res?.user?.uid), {
+                userName,
+                email,
+                userId: res?.user?.uid
+            })
+            return { success: true, data: res?.user}
         } catch (error) {
-            
+            if(error.message == 'Firebase: Password should be at least 6 characters (auth/weak-password).') return { success: false, message: "Password should be at least 6 characters"}
+            if(error.message == 'Firebase: Error (auth/invalid-email).') return { success: false, message: "Enter valid email"}
+            if(error.message == 'Firebase: Error (auth/email-already-in-use).') return { success: false, message: "Email already in use"}
+            return { success: false, message: error.message}
         }
     }
 
     return (
-        <AuthContext.Provider value={{user, isAuthenticated, login, logOut, register}}>
+        <AuthContext.Provider value={{user, isAuthenticated, login, logout: logOut, register}}>
             { children }
         </AuthContext.Provider>
     )
